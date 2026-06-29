c8488e5c786b4c598639c477843b67153f71c7a0
jamesw383 <jamesw383@users.noreply.github.com>
2026-06-30 01:24:20 +0400

    Release MakePay 0.4.0

--- BTCPayServer.Plugins.MakePay/MakePayPlugin.cs
+++ BTCPayServer.Plugins.MakePay/MakePayPlugin.cs
@@ -10,7 +10,7 @@ namespace BTCPayServer.Plugins.MakePay;
 
 public class MakePayPlugin : BaseBTCPayServerPlugin
 {
-    public const string PluginVersion = "0.3.1";
+    public const string PluginVersion = "0.4.0";
     public static readonly PaymentMethodId MakePayPaymentMethodId = new("MAKEPAY");
 
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
